b7ee8cbdb2705547e53818d37e784ff8d71ddf1a
mineSGR <58854905+mineSGR@users.noreply.github.com>
2020-11-18 18:44:52 +0100

    Add files via upload

--- src/main/Start.java
+++ src/main/Start.java
@@ -22,6 +22,8 @@ import javax.swing.JTextField;
 
 public class Start {
 
+	//@author Sebastian Green
+	
 	public static class stock {
 		
 		public String name;
@@ -40,15 +42,13 @@ public class Start {
 		public BigDecimal sellPrice;
 	}
 	
-	public class TradingBot {
+	public static class TradingBot {
 
-		public ArrayList<ArrayList<main.Start.stock>> aktier;
 		public ArrayList<main.Start.stock[]> boughtAktie;
 		public ArrayList<Recipt> recipts;
 		public BigDecimal money;
 		
-		public TradingBot(ArrayList<ArrayList<stock>> aktier, ArrayList<stock[]> boughtAktie, ArrayList<Recipt> recipts, BigDecimal money) {
-			this.aktier = aktier;
+		public TradingBot(ArrayList<stock[]> boughtAktie, ArrayList<Recipt> recipts, BigDecimal money) {
 			this.boughtAktie = boughtAktie;
 			this.recipts = recipts;
 			this.money = money;
@@ -65,7 +65,7 @@ public class Start {
 	private static String password;
 	
 	private static Socket s;
-	private static PrintWriter writer;
+	private static PrintWriter txtWriter;
 	private static Scanner txtReader;
 	private static ObjectInputStream objectReader;
 	private static TradingBot tb;
@@ -79,6 +79,7 @@ public class Start {
 		factory.FrameFactory.finalize(frame);
 	}
 	
+	//Methoden skapar startmenyn på skärmen
 	public static void startmenu() {
 		tb = null;
 		JTextField username = (JTextField) factory.FrameFactory.constrainComponent(new JTextField("Användarnamn"), layout, 1, 0, 1, 1, 1.0, 0.0, GridBagConstraints.HORIZONTAL, GridBagConstraints.CENTER);
@@ -99,7 +100,7 @@ public class Start {
 					JOptionPane.showMessageDialog(frame, "Innehållet i fälten får inte vara tomma");
 				} else {
 					removeComponents(new Component[] {username, password, login, createAccount});
-					getInfo("CHECKLOGIN", uName, psw);
+					getInfo("LOGIN", uName, psw);
 				}
 			}
 		});
@@ -121,74 +122,94 @@ public class Start {
 		frame.add(password);
 		frame.add(login);
 		frame.add(createAccount);
-		frame.repaint();
+		frameFixer();
 	}
 	
+	//Methoden skapar menyn man får när man loggat in
 	public static void usermenu() {
-		JLabel namn = (JLabel) factory.FrameFactory.constrainComponent(new JLabel("Inloggad som: " + Start.username), layout, 0, 0, 1, 1, 0.2, 0.0, GridBagConstraints.HORIZONTAL, GridBagConstraints.CENTER);
+		JLabel namn = (JLabel) factory.FrameFactory.constrainComponent(new JLabel("Inloggad som: " + Start.username), layout, 1, 0, 1, 1, 0.2, 0.0, GridBagConstraints.HORIZONTAL, GridBagConstraints.CENTER);
 		JButton getMoney = (JButton) factory.FrameFactory.constrainComponent(new JButton("Kolla saldo"), layout, 1, 1, 1, 1, 0.5, 0.5, GridBagConstraints.BOTH, GridBagConstraints.CENTER);
-		JButton update = (JButton) factory.FrameFactory.constrainComponent(new JButton("Hämta info igen"), layout, 2, 1, 1, 1, 0.5, 0.5, GridBagConstraints.BOTH, GridBagConstraints.CENTER);
+		JButton update = (JButton) factory.FrameFactory.constrainComponent(new JButton("Hämta info igen"), layout, 1, 2, 1, 1, 0.5, 0.5, GridBagConstraints.BOTH, GridBagConstraints.CENTER);
+		JButton money = (JButton) factory.FrameFactory.constrainComponent(new JButton("Lägg in pengar"), layout, 1, 3, 1, 1, 0.5, 0.5, GridBagConstraints.BOTH, GridBagConstraints.CENTER);
+		JButton logout = (JButton) factory.FrameFactory.constrainComponent(new JButton("Logga ut"), layout, 2, 0, 1, 1, 0.5, 0.5, GridBagConstraints.BOTH, GridBagConstraints.CENTER);
 		getMoney.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				displayMoney(tb.money);
+				int köptAktieSize = 0;
+				for(int i = 0; i < tb.boughtAktie.size(); i++) {
+					for(int j = 0; j < tb.boughtAktie.get(i).length; j++) {
+						if(tb.boughtAktie.get(i)[j] != null) {
+							köptAktieSize++;
+						}
+					}
+				}
+				JOptionPane.showMessageDialog(frame, "Antal köpta aktier: " + köptAktieSize + "\nAntal kvitton: " + tb.recipts.size() + "\nPengar kvar: " + tb.money);
 			}
 		});
 		update.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				removeComponents(new Component[] {namn, getMoney, update, money, logout});
 				getInfo("LOGIN", Start.username, Start.password);
 				JOptionPane.showMessageDialog(frame, "Information hämtad");
 			}
 		});
-		
-		frame.add(namn);
-		frame.add(getMoney);
-		frame.repaint();
-	}
-	
-	private static void displayMoney(BigDecimal money) {
-		JLabel showMoney = (JLabel) factory.FrameFactory.constrainComponent(new JLabel("Saldo: " + money.toString()), layout, 1, 1, 1, 1, 0.5, 0.0, GridBagConstraints.HORIZONTAL, GridBagConstraints.CENTER);
-		JButton moveBack = (JButton) factory.FrameFactory.constrainComponent(new JButton("Tillbaka"), layout, 0, 0, 1, 1, 0.5, 0.5, GridBagConstraints.BOTH, GridBagConstraints.CENTER);
-		moveBack.addActionListener(new ActionListener() {
+		money.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				removeComponents(new Component[] {showMoney, moveBack});
-				usermenu();
+				removeComponents(new Component[] {namn, getMoney, update, money, logout});
+				getInfo("MONEY", Start.username, Start.password);
+				JOptionPane.showMessageDialog(frame, "Pengarna är tillagda");
 			}
 		});
-		frame.add(showMoney);
-		frame.add(moveBack);
-		frame.repaint();
+		logout.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				removeComponents(new Component[] {namn, getMoney, update, money, logout});
+				username = null;
+				password = null;
+				tb = null;
+				startmenu();
+			}
+		});
+		frame.add(namn);
+		frame.add(getMoney);
+		frame.add(update);
+		frame.add(money);
+		frame.add(logout);
+		frameFixer();
 	}
 	
+	//Methoden hanterar uppkopplingen till servern
 	private static void getInfo(String choose, String uName, String psw) {
 		boolean loggedIn = false;
 		try {
 			s = new Socket(ipadress, port);
-			writer = new PrintWriter(s.getOutputStream());
+			txtWriter = new PrintWriter(s.getOutputStream());
 			txtReader = new Scanner(s.getInputStream());
 			objectReader = new ObjectInputStream(s.getInputStream());
-			writer.println(choose);
-			writer.println(uName);
-			writer.println(psw);
-			writer.flush();
+			txtWriter.println(choose);
+			txtWriter.println(uName);
+			txtWriter.println(psw);
+			txtWriter.flush();
 			if(choose.equals("LOGIN")) {
 				String boll = txtReader.nextLine();
 				if(boll.equals("TRUE")) {
 					try {
-					loggedIn = true;
-					ArrayList<ArrayList<stock>> tmpAktier = (ArrayList<ArrayList<stock>>) objectReader.readObject();
-					ArrayList<stock[]> tmpBought = (ArrayList<stock[]>) objectReader.readObject();
-					ArrayList<Recipt> tmpRecipt = (ArrayList<Recipt>) objectReader.readObject();
-					BigDecimal tmpMoney = (BigDecimal) objectReader.readObject();
-					JOptionPane.showMessageDialog(frame, "INFO: " + tmpAktier.toString() + ", " + tmpBought.toString() + ", " + tmpRecipt.toString() + ", " + tmpMoney.toString());
-					//tb = new TradingBot(tmpAktier, tmpBought, tmpRecipt, tmpMoney);
+						loggedIn = true;
+						ArrayList<stock[]> tmpBought = (ArrayList<stock[]>) objectReader.readObject();
+						ArrayList<Recipt> tmpRecipt = (ArrayList<Recipt>) objectReader.readObject();
+						BigDecimal tmpMoney = (BigDecimal) new BigDecimal(txtReader.nextLine());
+						tb = new TradingBot(tmpBought, tmpRecipt, tmpMoney);
+						if(tmpBought == null || tmpRecipt == null || tmpMoney == null) {
+							tb = null;
+						}
 					} catch(Throwable t) {
-						JOptionPane.showMessageDialog(frame, t.toString());
+						JOptionPane.showMessageDialog(frame, "Fel när informationen skulle laddas ner: " + t.toString());
 					}
 				}
 			} else if(choose.equals("CREATEACCOUNT")) {
@@ -198,31 +219,50 @@ public class Start {
 				} else {
 					JOptionPane.showMessageDialog(frame, "Kontot kunde inte skapas");
 				}
+			} else if(choose.equals("MONEY")) {
+				String input = JOptionPane.showInputDialog(frame, "Skriv hur mycket du vill lägga in");
+				int tal = 0;
+				try {
+					tal = Integer.parseInt(input);
+				} catch(Throwable t) {
+					JOptionPane.showMessageDialog(frame, "Felmeddelande: " + t.toString());
+				}
+				txtWriter.println(tal);
+				txtWriter.flush();
 			}
 		} catch (Throwable t) {
 			JOptionPane.showMessageDialog(frame, "Kunde inte logga in, felmeddelande: " + t.toString());
 			startmenu();
 		}
-		try {s.close();} catch (Throwable t) {} s = null; writer = null; txtReader = null; objectReader = null;
-		if(loggedIn) {
+		try {s.close();} catch (Throwable t) {} s = null; txtWriter = null; txtReader = null; objectReader = null;
+		if(loggedIn && tb != null) {
 			Start.username = uName;
 			Start.password = psw;
+			usermenu();
+		} else if(choose.equals("MONEY")) {
 			usermenu();
 		} else {
 			startmenu();
 		}
 	}
 	
+	//Methoden loggar ut användaren
 	private static void logout(Component[] comp) {
 		removeComponents(comp);
-		s = null; writer = null; txtReader = null; Start.username = null; Start.password = null;
+		s = null; txtWriter = null; txtReader = null; Start.username = null; Start.password = null;
 		startmenu();
 	}
 	
+	//Methoden ser till att fönstret är rent så att nytt innehåll kan målas på den
 	private static void removeComponents(Component[] comp) {
 		for(int i = 0; i < comp.length; i++) {
 			frame.remove(comp[i]);
 		}
+	}
+	
+	//Methoden uppdaterar fönstret
+	private static void frameFixer() {
 		frame.repaint();
+		frame.setVisible(true);
 	}
 }
